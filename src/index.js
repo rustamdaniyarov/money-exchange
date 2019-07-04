af9230320a4894a28cb845e7a1479f0553d323b6
rustamdaniyarov <52420729+rustamdaniyarov@users.noreply.github.com>
2019-07-04 10:57:10 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,35 +2,39 @@
 module.exports = function makeExchange(currency) {
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
-    var H = Math.floor(currency/50);
+    var t = "";
+	if (currency < 1){}
+	else if (currency > 10000){t = "error: \"You are rich, my friend! We don\'t have so much coins for exchange\""}
+	else {
+	var H = Math.floor(currency/50);
 	var Q = Math.floor((currency - H * 50)/25);
 	var D = Math.floor((currency - (H * 50 + Q * 25))/10);
 	var N = Math.floor((currency - (H * 50 + Q * 25 + D * 10))/5);
-	var P = Math.floor((currency - (H * 50 + Q * 25 + D * 10 + N * 5))/1);
-	var t = "";
+	var P = Math.floor((currency - (H * 50 + Q * 25 + D * 10 + N * 5))/1);	
 	if (H > 0) {
-		t = t + "\"H\":" + H;
+		t = t + "\"H\": " + H;
 	}
 	if (Q > 0 && t.length > 0) {
-		t = t + "," + "\"Q\":" + Q;
-	}else if (Q > 0 || t.length < 0){
-		t = t + "\"Q\":" + Q;
+		t = t + "," + "\"Q\": " + Q;
+	}else if (Q > 0 ){
+		t = t + "\"Q\": " + Q;
 	}
 	if (D > 0 && t.length > 0) {
-		t = t + "," + "\"D\":" + D;
-	}else if (D > 0 || t.length < 0){
-		t = t + "\"D\":" + D;
+		t = t + "," + "\"D\": " + D;
+	}else if (D > 0 ){
+		t = t + "\"D\": " + D;
 	}
 	if (N > 0 && t.length > 0) {
-		t = t + "," + "\"N\":" + N;
-	}else if (N > 0 || t.length < 0){
-		t = t + "\"N\":" + N;
+		t = t + "," + "\"N\": " + N;
+	}else if (N > 0 ){
+		t = t + "\"N\": " + N;
 	}
 	if (P > 0 && t.length > 0) {
-		t = t + "," + "\"P\":" + P;
-	}else if (P > 0 || t.length < 0){
-		t = t + "\"P\":" + P;
-	}
-	
+		t = t + "," + "\"P\": " + P;
+	}else if (P > 0 ){
+		t = t + "\"P\": " + P;
+	}}
+	//if (t.length>0)t = t + " ";
+	//t = "\{" + t + "\}";
 	return t;
 }
