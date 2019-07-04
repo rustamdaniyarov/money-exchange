20877d42b9a37a8d8444d650eb5de7c9fd4a8e64
rustamdaniyarov <52420729+rustamdaniyarov@users.noreply.github.com>
2019-07-04 11:02:22 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,27 +12,27 @@ module.exports = function makeExchange(currency) {
 	var N = Math.floor((currency - (H * 50 + Q * 25 + D * 10))/5);
 	var P = Math.floor((currency - (H * 50 + Q * 25 + D * 10 + N * 5))/1);	
 	if (H > 0) {
-		t = t + "\"H\": " + H;
+		t = t + "\"H\":" + H;
 	}
 	if (Q > 0 && t.length > 0) {
-		t = t + "," + "\"Q\": " + Q;
+		t = t + "," + "\"Q\":" + Q;
 	}else if (Q > 0 ){
-		t = t + "\"Q\": " + Q;
+		t = t + "\"Q\":" + Q;
 	}
 	if (D > 0 && t.length > 0) {
-		t = t + "," + "\"D\": " + D;
+		t = t + "," + "\"D\":" + D;
 	}else if (D > 0 ){
-		t = t + "\"D\": " + D;
+		t = t + "\"D\":" + D;
 	}
 	if (N > 0 && t.length > 0) {
-		t = t + "," + "\"N\": " + N;
+		t = t + "," + "\"N\":" + N;
 	}else if (N > 0 ){
-		t = t + "\"N\": " + N;
+		t = t + "\"N\":" + N;
 	}
 	if (P > 0 && t.length > 0) {
-		t = t + "," + "\"P\": " + P;
+		t = t + "," + "\"P\":" + P;
 	}else if (P > 0 ){
-		t = t + "\"P\": " + P;
+		t = t + "\"P\":" + P;
 	}}
 	//if (t.length>0)t = t + " ";
 	//t = "\{" + t + "\}";
